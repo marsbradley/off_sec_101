66f341c6c7b1982716a3e7d60deaada02bea7850
Damon Li <dli@expedia.com>
2018-02-11 15:36:56 +0000

    Initial commit for exercises

---
+++ assets/sqli_exercises/docker/webserver/src/index.php
@@ -0,0 +1,9 @@
+<html>
+	<head></head>
+<body>
+	<h2>Offensive Security SQL Injection Exercises</h2>
+	<a href="exercise_1.php">Exercise 1</a><br />
+	<a href="exercise_2.php">Exercise 2</a><br />
+	<a href="exercise_3.php">Exercise 3</a><br />
+</body>
+</html>
